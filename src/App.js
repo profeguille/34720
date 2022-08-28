dc5aae46df5acdff63db3dbfa8a1e5d8d61e413a
profeguille <guillermofergnani@gmail.com>
2022-08-28 19:31:02 -0300

    promesas

--- src/App.js
+++ src/App.js
@@ -3,29 +3,27 @@ import Bienvenida from './components/Bienvenida';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import Producto from './components/Producto';
+import Promesas from './test/Promesas';
 
 function App() {
-  let producto1 = { id: 100, name: 'zapato nike', price: 100 };
+  /* let producto1 = { id: 100, name: 'zapato nike', price: 100 };
   let producto2 = { id: 101, name: 'zapato adidas', price: 150 };
   let producto3 = { id: 102, name: 'vestido lola', price: 300 };
 
   function consoleHolaMundo() {
     console.log('hola mundo');
-  }
+  } */
 
   return (
     <div>
-      <NavBar />
+      <Promesas />
+      {/* <NavBar />
 
       <Bienvenida />
 
       <Producto item={producto1} color={'coral'} consoleHolaMundo={consoleHolaMundo} />
 
-      {/*       <Producto item={producto2} color={'blue'} consoleHolaMundo={consoleHolaMundo} />
-       */}
-      {/*       <Producto item={producto3} color={'blue'} consoleHolaMundo={consoleHolaMundo} />
-       */}
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
